ac8d882abec9da57ae2434f6efccd7507260ccb9
Cosmin Păunică <cosmin_paunica@yahoo.com>
2020-11-04 16:19:57 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Templul Cărților</title>
+	</head>
+	<body>
+		<h1>Templul Cărților</h1>
+
+		<section>
+			<h2>Prezentare generală</h2>
+			<p>Acest website va avea rolul de a ușura interacțiunea clienților cu o bibliotecă. Site-ul nu se va preocupa cu gestionarea activităților interne ale bibliotecii, precum angajați, salarii etc.</p>
+			<p>Pe acest site, vor exista conturi de tip admin, bibliotecar, client și utilizator simplu. Oricine își poate face cont de utilizator simplu. Bibliotecarii pot transforma un cont de utilizator simplu în cont de client (bibliotecarul crează un abonament pentru persoana ce deține contul), iar adminii vor putea transforma un cont de utilizator simplu sau de client în cont de bibliotecar (când bibliotecarul sau adminul este angajat) sau de admin.</p>
+		</section>
+
+		<section>
+			<h2>Tipuri de utilizatori</h2>
+			<ul>
+				<li>
+					<section>
+						<h3>Utilizatori simpli</h3>
+						<p>Utilizatorii simpli vor putea:</p>
+						<ul>
+							<li>Să vadă ce cărți aparțin bibliotecii.</li>
+							<li>Să vadă câte exemplare ale unei cărți sunt disponibile în bibliotecă.</li>
+							<li>În cazul cărților pentru care toate exemplarele sunt împrumutate, să vadă când este data de predare a unui exemplar (acest lucru nu va fi foarte precis, întrucât clienții își pot prelungi împrumuturile sau pot aduce cărțile înapoi mai devreme).</li>
+						</ul>
+					</section>
+				</li>
+				<li>
+					<section>
+						<h3>Clienți</h3>
+						<p>Pe lângă acțiunile specifice unui utilizator simplu, clienții vor putea:</p>
+						<ul>
+							<li>Să vadă cărțile care sunt în posesia lor la un moment dat și să vadă termenele de predare ale acestora.</li>
+							<li>Să rezerve cărți timp de un anumit număr de zile. Dacă nu onorează rezervarea, își pierd dreptul de a rezerva pentru o perioadă.</li>
+							<li>Să scrie recenzii ale cărților împrumutate, după ce le returnează.</li>
+						</ul>
+					</section>
+				</li>
+				<li>
+					<section>
+						<h3>Bibliotecari</h3>
+						<p>Pe lângă activitățile specifice clienților, bibliotecarii vor putea:</p>
+						<ul>
+							<li>Să marcheze un cont de utilizator simplu ca fiind cont de client.</li>
+							<li>Să înregistreze împrumutul unei cărți unui client, lucru ce va schimba informații de pe site, precum numărul de exemplare ale cărții disponibile în biblitoecă.</li>
+						</ul>
+					</section>
+				</li>
+				<li>
+					<section>
+						<h3>Admini</h3>
+						<p>Pe lângă activitățile specifice bibliotecarilor, adminii vor putea:</p>
+						<ul>
+							<li>Să marcheze un cont ca fiind cont de bibliotecar sau de admin.</li>
+							<li>Să adauge o nouă carte sau să modifice stocul unei cărți din bibliotecă.</li>
+						</ul>
+					</section>
+				</li>
+			</ul>
+		</section>
+
+		<section>
+			<h2>Baza de date</h2>
+		</section>
+	</body>
+</html>
