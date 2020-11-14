e742c5c92bbaabb6a1bac8af9c39cc57b68103b8
Cosmin Păunică <57110774+cosmin-paunica@users.noreply.github.com>
2020-11-14 20:19:37 +0200

    Update conexiune_bd.php

--- _setup/conexiune_bd.php
+++ _setup/conexiune_bd.php
@@ -1,6 +1,6 @@
 <?php
 
-$bd = new mysqli("localhost", "root", "", "templulcartilor");
+$bd = new mysqli("eu-cdbr-west-03.cleardb.net", "b09347d1d0ad6c", "494031ba", "templulcartilor");
 
 if (!$bd)
     die("Eroare de conectare la baza de date!");
