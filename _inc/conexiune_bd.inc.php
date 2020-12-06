1f0714a241245b2562e9b7ccfee695e9c46c6526
Cosmin Păunică <cosmin_paunica@yahoo.com>
2020-12-06 15:20:55 +0200

    Modify gitignore

--- _inc/conexiune_bd.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$bd = new mysqli("eu-cdbr-west-03.cleardb.net", "b09347d1d0ad6c", "494031ba", "heroku_6c9179c8ba09937");
-
-if (!$bd)
-    die("Eroare de conectare la baza de date!");
-
-?>
